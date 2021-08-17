553ed2590cb1e238a55c79dac678f6896e3bffbf
MuhammadAli <maxmudov63869@gmail.com>
2021-08-17 16:31:45 +0500

    finish

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt ('Введите ваше имя')
+var age = prompt ('Введите ваше возраст')
+var sum = prompt ('6+3=')
+var min = prompt ('6-3=')
+var umn = prompt ('6*2=')
+var del = prompt ('6/3=')
+var dels = prompt ('6%3=')
+
+console.log ('Ваше имя: '+name);
+console.log ('Ваш возраст: '+age);
+console.log ('6 + 3 = 9 '+ '(Ваш ответ '+ sum+')');
+console.log ('6 - 3 = 3 '+ '(Ваш ответ '+ min+')');
+console.log ('6 * 3 = 18 '+ '(Ваш ответ '+ umn+')');
+console.log ('6 / 3 = 2 '+ '(Ваш ответ '+ del+')');
+console.log ('6 % 3 = 0 '+ '(Ваш ответ '+ dels+')');
